75106bd274a29dd9bdfcb3e585536cbe8eedcc49
yaseminozturk9 <48058898+yaseminozturk9@users.noreply.github.com>
2019-03-29 20:28:41 +0300

    Create uygulama01.py

---
+++ uygulama01.py
@@ -0,0 +1,42 @@
+#_author_='Yasemin Öztürk'
+#Uygulama0I
+
+sorular = ['Dünyadaki tüm yeni doğmuş bebekler La notasıyla ağlar.[D/Y]',
+           '-1,1 sayısı -1,01\'den daha büyüktür.[D/Y]',
+           'Uganda\'da yaşayan Buganda insanları Luganda konuşur.[D/Y]',
+           'Aynı anda nefes alman ve yutkunman mümkün değildir.[D/Y]',
+           'Dinozorlar olmasaydı kuşlar olmazdı.[D/Y]',]
+cevaplar = ['D','Y','D','D','D']
+puan = 0
+cevap = input((sorular[0]))
+if cevaplar[0] == cevap.upper():
+    print("Harika, cevabınız doğru!")
+    puan += 1
+else:
+    print("Maalesef, yanlış cevap!")
+cevap = input((sorular[1]))
+if cevaplar[1] == cevap.upper():
+    print("Harika, cevabınız doğru!")
+    puan +=1
+else:
+    print("Maalesef, yanlış cevap!")
+cevap = input((sorular[2]))
+if cevaplar[2] == cevap.upper():
+    print("Harika, cevabınız doğru!")
+    puan += 1
+else:
+    print("Maalesef, yanlış cevap!")
+cevap = input((sorular[3]))
+if cevaplar[3] == cevap.upper():
+    print("Harika, cevabınız doğru!")
+    puan += 1
+else:
+    print("Maalesef, yanlış cevap!")
+cevap = input((sorular[4]))
+if cevaplar[4] == cevap.upper():
+    print("Harika, cevabınız doğru!")
+    puan += 1
+else:
+    print("Maalesef, yanlış cevap!")
+
+    print("Tebrikler, testi tamamladınız. Toplam puanınız: "+str(puan*20))
